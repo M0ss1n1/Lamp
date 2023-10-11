5ddef84d1c4fbb8b1d62f9d38a46a4e02016993f
Thiago Mossini <125108541+M0ss1n1@users.noreply.github.com>
2023-10-11 16:11:56 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,38 @@
+
+const lampada = document.getElementById('lampada');
+const btn = document.getElementById('btn');
+let ligada = false;
+
+function ligarDesligarLampada() {
+  ligada = !ligada;
+  if (ligada) {
+    lampada.src = 'on.png';
+    btn.src = 'btn.png';
+    lampada.style.animation = "opacidade-animation .3s linear forwards";
+    lampada.classList.add('brilho');
+  } else {
+    lampada.src = 'off.png';
+      btn.src = 'btn.png';
+    lampada.style.animation = "opacidade-animation1 .3s linear forwards";
+    lampada.classList.remove('brilho'); 
+  }
+}
+
+function quebrarLampada() {
+  if (ligada) {
+    alert('Você quebrou a lâmpada!');
+    lampada.src = 'broke.png';
+    btn.removeEventListener('click', ligarDesligarLampada);
+    btn.removeEventListener('dbclick', quebrarLampada);
+    lampada.removeEventListener('dblclick', quebrarLampada);
+    lampada.classList.remove('brilho');
+
+    setTimeout(() => {
+      location.reload()
+    }, 1500);
+  }
+}
+
+btn.addEventListener('click', ligarDesligarLampada);
+btn.addEventListener('dblclick', quebrarLampada);
+lampada.addEventListener('dblclick', quebrarLampada);
